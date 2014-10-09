0161036092aff12ee94023a6a56899726e331bee
saasbook <saasbook@saasbook.(none)>
2014-10-09 11:26:43 -0300

    Create Rail app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '854f37fcc73fa0d5af12d790f6a166431cd10bddd37231c9a0493d0eb965608d28c4d1b592f136cdb4cad8b084de08bbfb2efd8a2b6f06f960a48508b4eb2a25'
